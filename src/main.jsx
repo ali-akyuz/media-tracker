b071efecf8e1c0ff24dff31c6e79522c54e980c1
ali-akyuz <ali-akyuz@outlook.com>
2026-03-15 02:40:14 +0200

    Vercel Analytics ve Firestore eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { DataProvider } from './context/DataContext.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <DataProvider>
       <App />
+      <Analytics />
     </DataProvider>
   </StrictMode>,
 )
